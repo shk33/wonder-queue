a6b1c504aa53652458936b8c132083c611234308
shk33 <m.coronel.seg@gmail.com>
2021-07-11 16:05:38 -0500

    Server up and running

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({
+      sucess: true
+  });
+});
+
+module.exports = router;
